b04dd441a51f18cef7c146f380e3e2fec1901768
gustavoneves12 <gustavo.neves12@gmail.com>
2015-04-17 10:32:24 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace vizkit3d_world
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace vizkit3d_world
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vizkit3d_world/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    vizkit3d_world::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
