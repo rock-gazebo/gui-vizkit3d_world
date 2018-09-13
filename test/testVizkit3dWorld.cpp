78ddea9a66abef48584d9f38720582276e3d9c89
Gabriel Alcantara <gabriel.alcantara@13robotics.com>
2018-09-13 16:29:52 -0300

    add unit test to load a simple sdf world

--- test/testVizkit3dWorld.cpp
+++ test/testVizkit3dWorld.cpp
@@ -4,22 +4,10 @@
 
 using namespace vizkit3d_world;
 
-BOOST_AUTO_TEST_CASE(it_should_not_crash_when_welcome_is_called)
+BOOST_AUTO_TEST_CASE(it_loads_correctly_a_sdf_world)
 {
-    vizkit3d_world::Vizkit3dWorld vizkit3d_world;
+    vizkit3d_world::Vizkit3dWorld vizkit3d_world("test_data/simple.world");
+    RobotVizMap robot_viz = vizkit3d_world.getRobotVizMap();
+    BOOST_TEST(robot_viz.size(),1);
+    BOOST_CHECK_EQUAL(robot_viz.begin()->first,"simple_model");
 }
-
-BOOST_AUTO_TEST_CASE(it_starts_sdf)
-{   
-    std::string path = "test_data/simple.world";
-    std::pair<std::string, int> sdf_string = getRobotModelString(
-        path, kdl_parser::ROBOT_MODEL_FORMAT::ROBOT_MODEL_AUTO);
-    sdf::SDFPtr sdf(new sdf::SDF);
-    sdf::init(sdf);
-}
-
-
-//BOOST_AUTO_TEST_CASE(it_loads_correctly_a_sdf_world)
-//{
-//    vizkit3d_world::Vizkit3dWorld vizkit3d_world("test_data/simple.world");
-//}
