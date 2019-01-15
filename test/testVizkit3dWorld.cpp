cdebc725be662b649430ea30a517331681757313
Gabriel <gabriel.alcantara@13robotics.com>
2019-01-15 16:48:25 -0200

    add unit test for multiple instances

--- test/testVizkit3dWorld.cpp
+++ test/testVizkit3dWorld.cpp
@@ -11,3 +11,15 @@ BOOST_AUTO_TEST_CASE(it_loads_correctly_a_sdf_world)
     BOOST_TEST(robot_viz.size(),1);
     BOOST_CHECK_EQUAL(robot_viz.begin()->first,"simple_model");
 }
+
+BOOST_AUTO_TEST_CASE(it_can_create_multiple_vizkit3_world)
+{
+    vizkit3d_world::Vizkit3dWorld vizkit3d_world("test_data/simple.world");
+    RobotVizMap robot_viz = vizkit3d_world.getRobotVizMap();
+    vizkit3d_world::Vizkit3dWorld vizkit3d_world_2("test_data/simple.world");
+    RobotVizMap robot_viz_2 = vizkit3d_world_2.getRobotVizMap();
+    BOOST_TEST(robot_viz.size(),1);
+    BOOST_CHECK_EQUAL(robot_viz.begin()->first,"simple_model");
+    BOOST_TEST(robot_viz_2.size(),1);
+    BOOST_CHECK_EQUAL(robot_viz_2.begin()->first,"simple_model");
+}
